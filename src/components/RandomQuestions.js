706e9511e0076cd3f595eaf67d1116f82ed95f10
Andy Alcantara <andyalcantara745@yahoo.com>
2019-03-24 22:30:15 -0400

    Add random unitl 20 number

--- src/components/RandomQuestions.js
+++ src/components/RandomQuestions.js
@@ -2,18 +2,25 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 import { connect } from 'react-redux';
+import {handleQuestions} from "../actions/shared";
 
 class RandomQuestions extends React.Component {
+
+    componentDidMount() {
+        const { onReceiveQuestions } = this.props;
+        onReceiveQuestions();
+    }
 
     generateIndex = () => {
         const { cards } = this.props;
 
         return Math.floor(Math.random() * cards.length)
-    }
+    };
 
     render() {
         const { cards } = this.props;
-        console.log(Math.floor(Math.random() * 100));
+        console.log(Math.floor(Math.random() * 20));
+        console.log(cards[`${Math.floor(Math.random() * 20)}`]);
         return (
             <View>
                 <Text>Random Questions</Text>
@@ -25,9 +32,19 @@ class RandomQuestions extends React.Component {
 
 function mapStateToProps({ cards }) {
 
+    console.log(cards);
+
     return {
         cards: Object.keys(cards).map(key => cards[key])
     }
 }
 
-export default connect(mapStateToProps)(RandomQuestions);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onReceiveQuestions: () => {
+            dispatch(handleQuestions());
+        },
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RandomQuestions);
