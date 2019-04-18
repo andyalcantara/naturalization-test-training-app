79b2914320301496971792bda8a79a024682fdbd
Andy Alcantara <andyalcantara745@yahoo.com>
2019-04-17 22:58:55 -0400

    Add Custom Buttom COmponent

---
+++ src/utils/CustomButtom.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+const CustomButtom = () => {
+    return (
+        <View>
+            <Text>Hello Mate</Text>
+        </View>
+    );
+};
+
+export default CustomButtom;
